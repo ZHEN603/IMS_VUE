7857db0c1c87bfef4be98a7ab4046ab645296d2d
ZHEN603 <a513515732@hotmail.com>
2024-01-18 18:24:55 +0000

    router gaurdian set token header

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+import { Message } from 'element-ui'
 import store from '@/store'
-import { getToken } from '@/utils/auth'
-
+import router from '@/router'
 // create an axios instance
 const service = axios.create({
-  //baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // baseURL: process.env.VUE_APP_BASE_API,
+  // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
@@ -19,7 +19,7 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers.Authorization = 'Bearer ${store.getters.token}'
+      config.headers.Authorization = `Bearer ${store.getters.token}`
     }
     return config
   },
@@ -53,6 +53,12 @@ service.interceptors.response.use(
   },
   async(error) => {
     console.log('err' + error) // for debug
+    if (error.response.status === 401) {
+      Message({ type: 'warning', message: 'token expired' })
+      await store.dispatch('user/logout')
+      router.push('/login')
+      return Promise.reject(error)
+    }
     Message({ type: 'error', message: error.message })
     return Promise.reject(error)
   }
