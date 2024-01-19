18cd2936eb4c348d4e7556879c8c786c6ce22162
ZHEN603 <a513515732@hotmail.com>
2024-01-19 18:04:42 +0000

    category apis

---
+++ src/api/category.js
@@ -0,0 +1,36 @@
+import request from '@/utils/request'
+
+export function getCategory() {
+  return request({
+    url: '/product/category'
+  })
+}
+
+export function addCategory(data) {
+  return request({
+    url: '/product/category',
+    method: 'post',
+    data
+  })
+}
+
+export function getCategoryDetail(id) {
+  return request({
+    url: `/product/category/${id}`
+  })
+}
+
+export function updateCategory(data) {
+  return request({
+    method: 'put',
+    url: `/product/category/${data.id}`,
+    data
+  })
+}
+
+export function delCategory(id) {
+  return request({
+    method: 'delete',
+    url: `/product/category/${id}`
+  })
+}
