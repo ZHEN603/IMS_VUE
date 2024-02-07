f52e9092f9fb6faa4d23ef550f24d7d6425c9e5e
Zhenyu Liang <a513515732@hotmail.com>
2024-02-07 19:32:16 +0000

    add user apis

--- src/api/user.js
+++ src/api/user.js
@@ -10,14 +10,51 @@ export function login(data) {
 
 export function getInfo() {
   return request({
-    url: 'user/profile',
+    url: '/user/profile',
     method: 'post'
   })
 }
 
-export function logout() {
+export function getUserList(params) {
   return request({
-    url: '/vue-element-admin/user/logout',
-    method: 'post'
+    url: '/user/page',
+    params
+  })
+}
+
+export function getUser(id) {
+  return request({
+    url: `/user/${id}`
+  })
+}
+
+export function delUser(id) {
+  return request({
+    url: `/user/${id}`,
+    method: 'delete'
+  })
+}
+
+export function addUser(data) {
+  return request({
+    url: `/user`,
+    method: 'post',
+    data
+  })
+}
+
+export function updateUser(id, data) {
+  return request({
+    url: `/user/${id}`,
+    method: 'put',
+    data
+  })
+}
+
+export function assignRoles(data) {
+  return request({
+    url: `/user/assignRoles`,
+    method: 'put',
+    data
   })
 }
