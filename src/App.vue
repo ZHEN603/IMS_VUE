ca23f928d9caee3d6925320415de9bfeece20ac4
Zhenyu Liang <a513515732@hotmail.com>
2024-02-23 19:47:21 +0000

    add app style

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,14 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+  .app-container{
+    padding: 20px 30px;
+  }
+@media (max-width: 800px) {
+  .app-container {
+    padding: 20px 0px;
+  }
+}
+</style>
