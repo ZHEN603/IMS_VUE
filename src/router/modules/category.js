9d05862d2f7c908310838c111668167967917fb5
Zhenyu Liang <a513515732@hotmail.com>
2024-02-24 19:48:39 +0000

    category router

--- src/router/modules/category.js
+++ src/router/modules/category.js
@@ -2,11 +2,10 @@ import layout from '@/layout'
 export default {
   path: '/category',
   component: layout,
-  name: 'Category',
   children: [{
     path: '',
     component: () => import('@/views/category'),
-    name: 'Category',
+    name: 'category',
     meta: {
       icon: 'tree',
       title: 'Category'
