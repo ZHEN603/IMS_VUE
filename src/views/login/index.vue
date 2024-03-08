764fe36a31f193e312f7f13e5917b26d60e2a033
Zhenyu Liang <a513515732@hotmail.com>
2024-03-08 20:02:37 +0000

    update login page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="login-container">
-    <div class="logo" />
+    <div class="logo" v-show="isVisible"/>
     <div class="form">
-      <h1>Login</h1>
+      <h1>IMS</h1>
+      <h2>Login</h2>
       <el-card shadow="never" class="login-card">
         <el-form ref="form" :model="loginForm" :rules="loginRules">
-          <el-form-item prop="id">
-            <el-input v-model="loginForm.id" placeholder="Please enter your ID" />
+          <el-form-item prop="email">
+            <el-input v-model="loginForm.email" placeholder="Please enter your email" />
           </el-form-item>
           <el-form-item prop="password">
             <el-input v-model="loginForm.password" show-password placeholder="Please enter your password" />
@@ -24,17 +25,18 @@ export default {
   name: 'Login',
   data() {
     return {
+      isVisible: window.innerWidth >800,
       loginForm: {
-        id: '',
+        email: '',
         password: ''
       },
       loginRules: {
-        id: [{
+        email: [{
           required: true,
-          message: 'Please enter your ID',
+          message: 'Please enter your Email',
           trigger: 'blur'
         }, {
-          message: 'Incorrect ID',
+          message: 'Incorrect Email',
           trigger: 'blur'
         }],
         password: [{
@@ -42,80 +44,128 @@ export default {
           message: 'Please enter your password',
           trigger: 'blur'
         }, {
-          min: 8,
+          min: 6,
           max: 50,
-          message: 'Passwprd error',
+          message: 'Password error',
           trigger: 'blur'
         }]
       }
     }
+  },
+  created() {
+    this.updateVisibility()
+    window.addEventListener('resize', this.updateVisibility)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.updateVisibility);
   },
   methods: {
     login() {
       this.$refs.form.validate(async(isOK) => {
         if (isOK) {
           await this.$store.dispatch('user/login', this.loginForm)
-          this.$router.push('/')
+          if(this.loginForm.password=='password'){
+            this.$router.push('/profile')
+          }else{
+            this.$router.push('/')
+          }
+
         }
       })
+    },
+    updateVisibility() {
+      this.isVisible = window.innerWidth > 800;
     }
   }
 }
 </script>
-<style lang="scss">
-  .login-container {
+<style scoped lang="scss">
+.login-container {
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
+  .logo {
+    flex: 3;
+    background: rgba(38, 72, 176) url(../../assets/common/login_back.png)
+      no-repeat center / cover;
+    border-top-right-radius: 60px;
     display: flex;
-    align-items: stretch;
-    height: 100vh;
-    .logo {
-      flex: 3;
-      background: rgba(38, 72, 176) url(../../assets/common/login_back.png)
-        no-repeat center / cover;
-      border-top-right-radius: 60px;
-      display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-      justify-content: center;
-      padding: 0 100px;
-      .icon {
-        background: url(../../assets/common/logo.png) no-repeat 70px center /
-          contain;
-        width: 300px;
-        height: 50px;
-        margin-bottom: 50px;
-      }
-      p {
-        color: #fff;
-        font-size: 18px;
-        margin-top: 20px;
-        width: 300px;
-        text-align: center;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center;
+    padding: 0 100px;
+    .icon {
+      background: url(../../assets/common/logo.png) no-repeat 70px center /
+        contain;
+      width: 300px;
+      height: 50px;
+      margin-bottom: 50px;
+    }
+    p {
+      color: #fff;
+      font-size: 18px;
+      margin-top: 20px;
+      width: 300px;
+      text-align: center;
+    }
+  }
+  .form {
+    flex: 4;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 176px;
+    .el-card {
+      border: none;
+      padding: 0;
+    }
+    h2 {
+      padding-left: 20px;
+      font-size: 24px;
+    }
+    h1 {
+      padding-left: 20px;
+    }
+    .el-input {
+      width: 350px;
+      height: 44px;
+      .el-input__inner {
+        background: #f4f5fb;
       }
     }
-    .form {
-      flex: 4;
+    .el-checkbox {
+      color:#606266;
+    }
+  }
+}
+@media (max-width: 800px) {
+  .login-container {
+    .logo {
+      flex: 0;
+    }
+    div.form {
+      flex: 1;
       display: flex;
       flex-direction: column;
       justify-content: center;
-      padding-left: 176px;
+      padding: 0px !important;
+      padding-left: 0px !important;
       .el-card {
         border: none;
         padding: 0;
       }
-      h1 {
-        padding-left: 20px;
+      h2 {
         font-size: 24px;
       }
-      .el-input {
-        width: 350px;
-        height: 44px;
-        .el-input__inner {
-          background: #f4f5fb;
-        }
-      }
-      .el-checkbox {
-        color:#606266;
+      h1 {
+        padding-left: 0px;
+        text-align: center;
       }
     }
+    ::v-deep div.form {
+      padding: 0px !important;
+      padding-left: 0px !important;
+    }
   }
+}
 </style>
